aa37196aa79fae4094ad34da55144300978ba79d
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-17 14:06:07 +0100

    Creada función logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -96,4 +96,14 @@ export const login = async (req, res) => {
   });
 };
 
+export const logout = async (req, res) => {
+    res.cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(0),
+    });
+    return res.sendStatus(200);
+  };
+  
+
   
