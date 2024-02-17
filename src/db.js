c349b2c32fd6dc523b588c31369ce492293395f4
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-17 14:02:08 +0100

    Creada función de conexión a  DB

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+import MONGODB_URI from './config.js';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log('DB connected');
+    } catch(err) {
+        console.log('Error connecting to the DB: ', err);
+    }
+}
