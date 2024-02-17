01f1134cfcf3f311d9a36fd20ffa07175f254ef8
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-17 14:15:37 +0100

    Configuración del servidor express y middlewares

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+
+import authRoutes from "./routes/auth.routes.js";
+import productsRoutes from "./routes/products.routes.js";
+import { FRONTEND_URL } from "./config.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: FRONTEND_URL
+  })
+);
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname
+    );
+  },
+});
+
+const fileFilter = (req, res, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'||
+    file.mimetype === 'image/webp'||
+    file.mimetype === 'image/avif'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api', productsRoutes);
+
+export default app;
