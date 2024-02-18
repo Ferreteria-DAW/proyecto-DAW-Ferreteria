013463d4b58cf105f1dd1cfd510c02f9b68f2cb9
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-18 15:40:58 +0100

    Solucionado error en App.jsx importado el ProductProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,29 @@ import { AuthProvider } from "./context/AuthContext";
 
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import HomePage from './pages/HomePage';
-import ProductsFormPage from './pages/ProductsFormPage';
-import ProductsPage from './pages/ProductsPage';
-import {Navbar} from './components/ui/Navbar';
+import HomePage from "./pages/HomePage";
+import ProductsFormPage from "./pages/ProductsFormPage";
+import ProductsPage from "./pages/ProductsPage";
+import { Navbar } from "./components/ui/Navbar";
+import { ProductProvider } from "./context/ProductsContext";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <ProductProvider>
+        <BrowserRouter>
           <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/add-product" element={<ProductsFormPage />} />
-          <Route path="/products/:id" element={<ProductsFormPage />} />
-          {/* <Route path="/profile" element={<h1>Profile</h1>} /> */}
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/add-product" element={<ProductsFormPage />} />
+            <Route path="/products/:id" element={<ProductsFormPage />} />
+            {/* <Route path="/profile" element={<h1>Profile</h1>} /> */}
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
     </AuthProvider>
   );
 }
