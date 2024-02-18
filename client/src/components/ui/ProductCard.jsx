da50a1edeeba24d89c02eaffc5651b065dcba1ae
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-18 15:38:06 +0100

    Añadida Card para productos y configurada página de productos

---
+++ client/src/components/ui/ProductCard.jsx
@@ -0,0 +1,27 @@
+import { useProduct } from "../../context/ProductsContext";
+import { Link } from "react-router-dom";
+
+export const ProductCard = ({ product }) => {
+  const { deleteProduct } = useProduct();
+
+  return (
+    <section className="bg-zinc-800 p-4 my-2 rounded-md">
+      <header className="flex justify-between">
+        <h2>{product.productName}</h2>
+      </header>
+      <img src={product.productImage} alt={product.productName} />
+      <p className=" text-slate-300">{product.productDescription}</p>
+      <p className=" text-slate-300">{product.productPrice}</p>
+      <div className="flex gap-x-2 items-center">
+        <button
+          onClick={() => {
+            deleteProduct(product._id);
+          }}
+        >
+          Eliminar
+        </button>
+        <Link to={`/product/${product._id}`}>Editar</Link>
+      </div>
+    </section>
+  );
+};
--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -1,9 +1,23 @@
-
+import { useEffect } from "react";
+import { useProduct } from "../context/ProductsContext";
+import { ProductCard } from "../components/ui/ProductCard";
 
 const ProductsPage = () => {
-  return (
-    <div>ProductsPage</div>
-  )
-}
+  const { products, getProducts } = useProduct();
 
-export default ProductsPage
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  products.length === 0 && <div>No hay productos</div>;
+
+  return(
+    <div className="flex flex-wrap gap-4">
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
+    </div>
+  )
+};
+
+export default ProductsPage;
