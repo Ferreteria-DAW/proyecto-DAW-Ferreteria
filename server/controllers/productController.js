c2b44520dc976c3ec092c06707a3155a2b9faac0
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 13:02:48 +0100

    Creada lógica editar produtos

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,4 @@
-const Post = require("../models/postModel");
+const Product = require("../models/productModel");
 const User = require("../models/userModel");
 const path = require("path");
 const fs = require("fs");
@@ -88,5 +88,63 @@ const getProductsByCategory = async (req, res, next) => {
         res.status(200).json(categoryProducts);
     } catch(err) {
         return next(new HttpError("Error al obtener los productos", 500));
+    }
+}
+
+/* Editar producto
+patch --> api/products/:id
+PROTEGIDA */
+
+const editProduct = async (req, res, next) => {
+    try {
+        let fileName;
+        let newFilename;
+        let updatedProduct;
+        const productId = req.params.id;
+        let { productName, productDescription,  category, productPrice } = req.body;
+
+        if(!productName || productDescription.length < 12 || !category || !productPrice) return next(new HttpError("Por favor ingrese todos los campos", 400));
+        
+        if(!req.files) {
+            updatedProduct = await Product.findByIdAndUpdate(productId, {
+                productName,
+                productDescription,
+                category,
+                productPrice,
+            }, { new: true });
+        } else {
+            const oldProduct = await Product.findById(productId);
+            fs.unlink(path.join(__dirname, '..', 'uploads', oldProduct.thumbnail), async (err) => {
+                if(err) return next(new HttpError("Error al editar el producto", 500));
+            });
+
+            const { thumbnail } = req.files;
+            if(thumbnail.size > 2000000) return next(new HttpError("La imagen es muy pesada, no debe superar lo 2Mb", 400));
+
+            fileName = thumbnail.name;
+            let splittedName = fileName.split(".");
+            newFilename = splittedName[0] + uuid() + "." + splittedName[splittedName.length - 1];
+            thumbnail.mv(path.join(__dirname, '..', 'uploads', newFilename), async (err) => {
+                if(err) return next(new HttpError("Error al subir la imagen", 500));
+
+                updatedProduct = await Product.findByIdAndUpdate(
+                    productId,
+                    {
+                        productName,
+                        productDescription,
+                        category,
+                        productPrice,
+                        thumbnail: newFilename,
+                    },
+                    { new: true }
+                );
+                if(!updatedProduct) return next(new HttpError("Error al editar el producto", 500));
+
+                res.status(200).json(updatedProduct);
+            })
+
+        }
+    } catch(err) {
+        return next(new HttpError("Error al editar el producto", 500));
     }
 }
--- server/models/productModel.js
+++ server/models/productModel.js
@@ -10,6 +10,10 @@ const productSchema = new Schema({
         type: String,
         required: true
     },
+    category: {
+        type: String,
+        required: true
+    },
     productPrice: {
         type: Number,
         required: true
