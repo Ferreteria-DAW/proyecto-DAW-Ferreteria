5cb047d5486704523dbdb919a8d39b4c48b9ade6
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 10:25:18 +0100

    Reestructurado proyecto --> Empezado de 0

--- client/src/components/ui/Anchor.jsx
+++
@@ -1,7 +0,0 @@
-import { Link } from 'react-router-dom';
-
-export const Anchor = ({to, children}) => {
-    <Link to={to} className=' bg-indigo-500 px-3 py-2 rounded-md'>
-        {children}
-    </Link>
-};
