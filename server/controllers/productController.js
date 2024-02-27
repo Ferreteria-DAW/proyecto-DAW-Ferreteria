bfeee506912253913296114c97de793c150eb162
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 12:40:33 +0100

    Creada lógica crear produto

---
+++ server/controllers/productController.js
@@ -0,0 +1,46 @@
+const Post = require("../models/postModel");
+const User = require("../models/userModel");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const HttpError = require("../models/errorModel");
+
+/* CREAR PRODUCTO
+post --> api/products 
+PROTEGIDA */
+
+const createProduct = async (req, res, next) => {
+    try {
+        let { productName, productDescription, productPrice } = req.body;
+        if(!productName || !productDescription || !productPrice || !req.files) return next(new HttpError("Por favor ingrese todos los campos", 400));
+
+        const { thumbnail } = req.files;
+
+        if(thumbnail.size > 2000000) return next(new HttpError("La imagen es muy pesada, no debe superar lo 2Mb", 400));
+        let fileName = thumbnail.name;
+        let splittedName = fileName.split(".");
+        let newFilename = splittedName[0] + uuid() + "." + splittedName[splittedName.length - 1];
+
+        thumbnail.mv(path.join(__dirname, '..', 'uploads', newFilename), async (err) => {
+            if(err) return next(new HttpError("Error al subir la imagen", 500));
+
+            else {
+                const newProduct = await productDescription.create({
+                    productName,
+                    productDescription,
+                    productPrice,
+                    thumbnail: newFilename,
+                    creator: req.user.id,
+                });
+
+                if(!newProduct) return next(new HttpError("Error al crear el producto", 500));
+
+                res.status(201).json(newProduct);
+               
+            }
+        })
+        
+    }catch(err) {
+        next(new HttpError("Error al crear el producto", 500));
+    }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,14 @@
-// const { Router } = require('express');
+const { Router } = require('express');
 
-// const { registerUser, loginUser, getUser, changeAvatar, editUser } = require('../controllers/userController');
-// const authMiddleware = require('../middleware/authMiddleware');
+const { registerUser, loginUser, getUser, changeAvatar, editUser } = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-// const router = Router();
+const router = Router();
 
-// router.post('/register', registerUser); //registro de usuario
-// router.post('/login', loginUser); //Inicio de sesión
-// router.get('/:id', getUser); //Perfil de usuario
-// router.post('/change-avatar', authMiddleware, changeAvatar); //Cambiar foto de perfil
-// router.patch('/edit-user', authMiddleware, editUser); //Editar usuario
+router.post('/register', registerUser); //registro de usuario
+router.post('/login', loginUser); //Inicio de sesión
+router.get('/:id', getUser); //Perfil de usuario
+router.post('/change-avatar', authMiddleware, changeAvatar); //Cambiar foto de perfil
+router.patch('/edit-user', authMiddleware, editUser); //Editar usuario
 
-// module.exports = router;
+module.exports = router;
