810a542332980200439e8b0a43c3924b15af214b
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 10:48:59 +0100

    Creado modelo usuario

---
+++ server/models/userModel.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String
+    },
+});
+
+module.exports = model('User', userSchema);
