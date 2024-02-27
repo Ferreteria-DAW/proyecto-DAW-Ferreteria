f72858d4c11f4f1207213bca7c1dd1851cf662ea
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 20:20:02 +0100

    Creado componente productItem

---
+++ client/src/components/ProductItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const ProductItem = ({
+    productId,
+    category,
+    productName,
+    productDescription,
+    thumbnail
+}) => {
+
+    const substrDescription = productDescription.length > 150 ? productDescription.substr(0, 150) + '...' : productDescription;
+    
+  return (
+    <article className='product'>
+        <div className="product__thumbnail">
+            <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${thumbnail}`} alt={productName} />
+        </div>
+        <div className="product__content">
+            <Link to={`/products/${productId}`}>
+                <h3>{productName}</h3>
+            </Link>
+        </div>
+        <p dangerouslySetInnerHTML={{ __html: substrDescription}} />
+
+        <div className="product__footer">
+            <Link to={`/products/categories/${category}`} className='btn category' >
+                {category}
+            </Link>
+        </div>
+    </article>
+  )
+}
+
+export default ProductItem
