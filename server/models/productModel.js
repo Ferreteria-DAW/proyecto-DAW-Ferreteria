5c43c524eeb56594fd0df32d12fdfcbe73ce434e
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-27 10:52:17 +0100

    Creado modelo Productos

---
+++ server/models/productModel.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+    productName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productDescription: {
+        type: String,
+        required: true
+    },
+    productPrice: {
+        type: Number,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: Schema.Tyoes.ObjectId, ref: 'User'
+    }
+}, { timestamps: true });
+
+module.exports = model('Product', productSchema);
