20a2d3987d894054b7da41806607e0d356ff4625
Adri <a.fernandezrodriguez2003@gmail.com>
2024-03-04 12:51:51 +0100

    Modificamos el js

--- client/src/toggle.js
+++ client/src/toggle.js
@@ -1,17 +1,15 @@
-/*var toggle = document.getElementById('boton-container');
+var toggle = document.getElementById('boton-container');
 
 function toggleMenu() {
   toggle.classList.toggle('active');
 }
 
-export default toggleMenu;*/
-
-let toggle = document.getElementById('boton-container');
-
-function toggleMenu() {
-  if (toggle) {
-    toggle.classList.toggle('active');
-  }
-}
-
 export default toggleMenu;
+
+// var toggle = document.getElementById('container');
+// var body = document.querySelector('body');
+
+// toggle.addEventListener('click', function() {
+//     toggle.classList.toggle('active');
+//   body.classList.toggle('active');
+// });
